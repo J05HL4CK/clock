0bb795c8981ec4ba498948bb400892d59bbf08e2
J05HL4CK <joshualack994@gmail.com>
2022-08-26 22:43:39 +1000

    gui made with kivy, time placeholder and buttons added to interface. Minimal styling added

---
+++ clock.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+import datetime
+
+
+class StudyTimer(App):
+    pass
+
+
+
+if __name__ == "__main__":
+    StudyTimer().run()
