1323186be3dc1f5cf01cf2d32617e2cb702f9ede
J05HL4CK <joshualack994@gmail.com>
2022-08-26 23:47:00 +1000

    buttons spaced evenly, rgba color values added for state changes. window size configd

--- clock.py
+++ clock.py
@@ -3,7 +3,13 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
 import datetime
+from kivy.config import Config
 
+
+Config.set('graphics', 'height', '600')
+Config.set('graphics', 'width', '300')
+Config.set('graphics', 'resizable', '0')
+Config.write()
 
 class StudyTimer(App):
     pass
