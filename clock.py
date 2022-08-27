933ab1543522f46dccf6986f2db1e25c3f6bf9f4
J05HL4CK <joshualack994@gmail.com>
2022-08-27 16:21:41 +1000

    countdown function added, code refactored

--- clock.py
+++ clock.py
@@ -1,20 +1,72 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from time import strftime
 from kivy.clock import Clock
-import datetime
 from kivy.config import Config
 
-
-Config.set('graphics', 'height', '600')
-Config.set('graphics', 'width', '300')
+Config.set('graphics', 'width', '500')
+Config.set('graphics', 'height', '300')
 Config.set('graphics', 'resizable', '0')
 Config.write()
 
-class StudyTimer(App):
-    pass
+# countdown timer
+# take a time when the start button is pressed T1
+# take the time when the stop button is pressed T2
+# take the difference between the start and stop
+
+class StudyClock(App):
+
+    default_period = 1500
+    focus_period = default_period
+    break_period = 300
+    rest_period = 900
+    timer_on = False
+
+
+    def format_time(self):
+        """return formatted string of mins and secs"""
+        self.mins, self.secs = divmod(self.focus_period,60)
+        time = f"{self.mins}:{self.secs}"
+        return(time)
+
+
+    def toggle_state(self):
+        """Toggle the state of timer on/off"""
+        self.timer_on =  not self.timer_on
+
+
+    def reset(self):
+        """Time off and period reset to default"""
+        self.timer_on = False
+        self.focus_period = self.default_period
 
 
 
-if __name__ == "__main__":
-    StudyTimer().run()
+    def update_time(self,delta):
+        self.root.ids.time.text = strftime('[b]%I:%M[/b]:%S %p')
+        if self.timer_on and self.focus_period > 0:
+            self.focus_period -= delta
+            mins, secs = divmod(self.focus_period,60)
+            self.root.ids.counter.text = f'{int(mins):02}:{int(secs):02}'
+        elif self.focus_period == 0:
+            self.timer_On = False
+
+
+
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 0)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    from kivy.core.window import Window
+    from kivy.utils import get_color_from_hex
+    
+    Window.clearcolor = get_color_from_hex('#010101')
+    
+    StudyClock().run()
